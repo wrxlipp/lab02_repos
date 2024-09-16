44441995010550ab132d58790b8153f2c8417a57
wrxlipp <110350889+wrxlipp@users.noreply.github.com>
2024-09-16 19:32:52 +0300

    first branch_02 commit

--- main.cpp
+++ main.cpp
@@ -9,21 +9,21 @@ using namespace std;
 int main()
 {
     double alpha;
-    double z1;
-    //double z2;
+    //double z1;
+    double z2;
 
 
     cout << "alpha (in radians) = ";
     cin >> alpha;
 
 
-    z1 = cos(alpha) + sin(alpha) + cos(3 * alpha) + sin(3 * alpha);
-    //z2 = 2 * sqrt(2) * cos(alpha) * sin(PI / 4 + 2 * alpha);
+    //z1 = cos(alpha) + sin(alpha) + cos(3 * alpha) + sin(3 * alpha);
+    z2 = 2 * sqrt(2) * cos(alpha) * sin(PI / 4 + 2 * alpha);
 
 
     cout << endl;
-    cout << "z1 = " << z1 << endl;
-    //cout << "z2 = " << z2 << endl;
+    //cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
 
     return 0;
 }
